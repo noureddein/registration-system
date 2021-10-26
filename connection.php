20c9f56e8f1778fd454475b7df1b28714ff84fcb
noureddein <noureddein@gmail.com>
2021-10-26 21:22:42 +0300

    change connections values

--- connection.php
+++ connection.php
@@ -1,10 +1,16 @@
 <?php
 
-// LOcal Database
-$serverName = 'localhost';
-$dbname = 'regForm';
-$dbUser = 'root';
-$dbPass = 'root';
+// Local Database
+// $serverName = 'localhost';
+// $dbname = 'regForm';
+// $dbUser = 'root';
+// $dbPass = 'root';
+
+//Remote Connection
+$serverName = 'remotemysql.com';
+$dbname = 'em8uEEBJj6';
+$dbUser = 'em8uEEBJj6';
+$dbPass = 'uBOGVOBBY5';
 
 $db = mysqli_connect($serverName, $dbUser, $dbPass, $dbname) or die(mysqli_error($db));
 // Get Heroku ClearDB connection information
